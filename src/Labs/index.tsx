7364f24e69b167e3d05152fb2d5be35095baef80
leejohnnn <lee.joh@northeastern.edu>
2025-01-14 21:05:33 -0500

    added name and section

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -8,6 +8,7 @@ export default function Labs() {
   return (
     <div>
       <h1>Labs</h1>
+      <h2>John Lee Section 02</h2>
       <TOC />
       <Routes>
         <Route path="/" element={<Navigate to="Lab1" />} />
